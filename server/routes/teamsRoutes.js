0652ba65216ec6a40493b29ab401154f41fd678f
Enrique Barco <enrique.barco96@hotmail.com>
2022-01-19 18:29:48 -0600

    Added final starter packages including bcrypt and jsonwebtoken

---
+++ server/routes/teamsRoutes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+// const knex = require("../knexConfig");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
---
+++ server/routes/usersRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+// const knex = require("../knexConfig");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// create a new user
+router.post("./register", (req, res) => {
+
+});
+
+// user login
+router.post("/login", (req, res) => {
+
+});
