81000bdedc374c78abe95f7aa0e3ef3910f60ec8
Enrique Barco <enrique.barco96@hotmail.com>
2022-01-27 20:32:50 -0500

    header styling complete

--- client/src/components/PageHeader/PageHeader.jsx
+++ client/src/components/PageHeader/PageHeader.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link} from 'react-router-dom';
+import Logo from "../../assets/Logo/Logo Files/For Web/svg/Color logo - no background.svg"
+import 'animate.css';
+import "./PageHeader.scss";
 
 export default function PageHeader() {
   return (
       <header className="header">
-              <img src="" alt="" className="header__logo" />
+              <Link to="/">
+                <img src={Logo} alt="unbound logo" className="header__logo" />
+              </Link>
           <nav className="header__nav-container">
-              <NavLink to="/contract">
+              <NavLink to="/"  className="header__link">
+                <h3 className="animated header__nav-item">Dashboard</h3>
+              </NavLink>
+              <NavLink to="/contract" className="header__link">
                 <h3 className="header__nav-item">Contracts</h3>
               </NavLink>
-              <NavLink to="/payments">
-                <h3 className="header__nav-item">Payments</h3>
-              </NavLink>
-              <NavLink to="/profile">
+              <NavLink to="/profile" className="header__link">
                 <h3 className="header__nav-item">Profile</h3>
               </NavLink>
           </nav>
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import "./index.scss";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY)
 
