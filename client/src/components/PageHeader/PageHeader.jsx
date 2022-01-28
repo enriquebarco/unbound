4013e7e1ad6ae582eff04492a17057821ed0462d
Enrique Barco <enrique.barco96@hotmail.com>
2022-01-27 20:48:21 -0500

    final touches

--- client/src/components/PageHeader/PageHeader.jsx
+++ client/src/components/PageHeader/PageHeader.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavLink, Link} from 'react-router-dom';
 import Logo from "../../assets/Logo/Logo Files/For Web/svg/Color logo - no background.svg"
-import 'animate.css';
 import "./PageHeader.scss";
 
 export default function PageHeader() {
@@ -12,7 +11,7 @@ export default function PageHeader() {
               </Link>
           <nav className="header__nav-container">
               <NavLink to="/"  className="header__link">
-                <h3 className="animated header__nav-item">Dashboard</h3>
+                <h3 className="header__nav-item">Dashboard</h3>
               </NavLink>
               <NavLink to="/contract" className="header__link">
                 <h3 className="header__nav-item">Contracts</h3>
