5292a3db37beb19a7a8852e639ff486ba466ec3f
Enrique Barco <enrique.barco96@hotmail.com>
2022-01-29 14:42:04 -0500

    styling completed for add contract form

--- client/src/components/AddContractForm/AddContractForm.jsx
+++ client/src/components/AddContractForm/AddContractForm.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import Input from "../../components/Input/Input";
+import Icon from "../../assets/Icons/contract.png"
+import "./AddContractForm.scss";
 
 export default function AddContactForm( { handleForm } ) {
   return(
     <main className="add-contract__container">
         <form className="add-contract__form" onSubmit={handleForm}>
-            <h1 className="signup__title">Create a new contract</h1>
+            <h1 className="add-contract__title">Create a new contract</h1>
 
-            <Input type="text" name="name" label="Full name" />
-            <Input type="text" name="country" label="Country" />
-            <Input type="date" name="startDate" label="Start Date" />
-            <Input type="date" name="endDate" label="End Date" />
-            <Input type="number" name="terminationPeriod" label="Termination Period" />
-            <Input type="text" name="jobTitle" label="Job Title" />
-            <Input type="text" name="milestone" label="Milestone" />
-            <Input type="text" name="milestoneDescription" label="Milestone Description" />
-            <Input type="text" name="prefCurrency" label="Preferred Currency" />
-            <Input type="text" name="paymentAmount" label="Payment Amount" />
+            <div className="add-contract__column add-contract__column--primary ">
+              <Input type="text" name="name" label="Full name" />
+              <Input type="text" name="country" label="Country" />
+              <Input type="date" name="startDate" label="Start Date" />
+              <Input type="date" name="endDate" label="End Date (optional)" />
+              <Input type="number" name="terminationPeriod" label="Termination Period (days)" />
+            </div>
 
-            <button className="add-contract__button">Create Contract</button>
+            <div className="add-contract__column">
+              <Input type="text" name="jobTitle" label="Job Title" />
+              <Input type="text" name="milestone" label="Milestone" />
+              <Input type="text" name="milestoneDescription" label="Milestone Description" />
+              <Input type="text" name="prefCurrency" label="Preferred Currency" />
+              <Input type="text" name="paymentAmount" label="Payment Amount ($US)" />
+            </div>
+
+            <div className="add-contract__submit-wrapper">
+              <img src={Icon} alt="" className="add-contract__icon" />
+              <button className="add-contract__button">Create Contract</button>
+              <img src={Icon} alt="" className="add-contract__icon" />
+            </div>
         </form>
     </main>
   )
