56bc2bf8c736c55d97aff9fc39df4b4c58d4faf8
Enrique Barco <enrique.barco96@hotmail.com>
2022-01-30 15:55:48 -0500

    final branch commit

--- client/src/components/PaymentListContainer/PaymentListContainer.jsx
+++ client/src/components/PaymentListContainer/PaymentListContainer.jsx
@@ -3,12 +3,14 @@ import PaymentListHeading from '../PaymentListHeading/PaymentListHeading';
 import PaymentList from "../PaymentsList/PaymentsList";
 import TeamProfile from '../TeamProfile/TeamProfile';
 import "./PaymentListContainer.scss";
+import 'animate.css';
+
 
 export default function PaymentListContainer( { data } ) {
   return (
         <main className="payment-container">
             <TeamProfile data={data} />
-            <div className="payment-container__list">
+            <div className="payment-container__list animate__animated animate__fadeInRight">
             <PaymentListHeading />
             <PaymentList data={data} />
             </div>
--- client/src/components/TeamProfile/TeamProfile.jsx
+++ client/src/components/TeamProfile/TeamProfile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Icon from "../../assets/Icons/user.png"
 import "./TeamProfile.scss"
+import 'animate.css';
 
 export default function TeamProfile( { data } ) {
   return(
-      <main className="team-profile">
+      <main className="team-profile animate__animated animate__fadeInLeft">
           <h1 className="team-profile__name">{data[0].name}</h1>
           <img src={Icon} alt="profile icon" className="team-profile__icon" />
           <h2 className="team-profile__label">Job Title</h2>
